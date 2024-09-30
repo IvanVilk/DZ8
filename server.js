158383b7b28ce2f9c2754bc93047bd7c00ffaf16
Ivan Vilkov <iv.vlkv.88@gmail.com>
2024-09-30 17:52:58 +0300

    Установлены необходимые библиотеки и создан файл .gitignore

---
+++ server.js
@@ -0,0 +1,9 @@
+const math = require('./math');
+
+console.log('Привет!')
+
+const result = math.add(10, 1);
+console.log('Результат выполнения функции add:', result)
+
+const result2 = math.subtract(1000, 1);
+console.log('Результат выполнения функции subtract:', result2)
